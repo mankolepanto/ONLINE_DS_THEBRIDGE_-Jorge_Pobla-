288d9af7b3e7c2d7b3740ad5dfe5a33739d200dd
mankolepanto <40011445+mankolepanto@users.noreply.github.com>
2024-04-26 15:01:59 +0200

    cosas

---
+++ EDA/btc/librerias.py
@@ -0,0 +1,18 @@
+import datetime as dt
+import pandas as pd
+import numpy as np
+
+from pandas_datareader import data as pdr
+import plotly.offline as pyo
+import plotly.graph_objects as go
+from plotly.subplots import make_subplots
+
+
+import matplotlib.pyplot as plt
+import pandas as pd
+
+import seaborn as sns
+import yfinance as yf
+
+
+from pandas_datareader import data as pdr
